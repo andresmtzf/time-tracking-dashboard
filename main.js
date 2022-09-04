847d98ffbcf501bca7d534dcf8a94d774fcd9d1c
Andres Martinez <andres_mf1@live.com.mx>
2022-09-04 01:54:55 -0500

    update onload

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ let backgroundColors = [
     {activity: 'self care', color: 'hsl(43, 84%, 65%)'},
 ]
 
-let dailyArray = data.map(item => item.timeframes.daily);
-let weeklyArray = data.map(item => item.timeframes.weekly);
-let monthlyArray = data.map(item => item.timeframes.monthly);
+const dailyArray = data.map(item => item.timeframes.daily);
+const weeklyArray = data.map(item => item.timeframes.weekly);
+const monthlyArray = data.map(item => item.timeframes.monthly);
 
-let dailyBtn = document.querySelector('#daily');
-let weeklyBtn = document.querySelector('#weekly');
-let monthlyBtn = document.querySelector('#monthly');
+const dailyBtn = document.querySelector('#daily');
+const weeklyBtn = document.querySelector('#weekly');
+const monthlyBtn = document.querySelector('#monthly');
 
 let secondSection = document.querySelector('.second-section');
 
@@ -38,34 +38,40 @@ monthlyBtn.addEventListener('click', () => {
     drawElements(monthlyArray);
 })
 
-function drawElements(array){
-    secondSection.innerHTML = '';
-    array.forEach((element, index) => {
-        let activity = data[index].title;
-        let activityLowerCase = activity.toLocaleLowerCase();
 
-        //Buscar color
-        let colorObject = backgroundColors.find(element => element.activity === activityLowerCase);        
-
-        if(activityLowerCase === 'self care'){ activityLowerCase = 'self-care'}
-
-        secondSection.innerHTML +=
-
-            `<div class="card">
-                <div class="card__background" style="background-color: ${colorObject.color};">
-                    <img class="card__image" src="./images/icon-${activityLowerCase}.svg" alt="${activityLowerCase}-ico" />
-                </div>
-                <div class="card__details">
-                    <div class="card__activity">
-                        <p class="card__title">${activity}</p>
-                        <img src="./images/icon-ellipsis.svg" alt="menu three dots" />
+    function drawElements(array){
+        secondSection.innerHTML = '';
+        array.forEach((element, index) => {
+            let activity = data[index].title;
+            let activityLowerCase = activity.toLocaleLowerCase();
+    
+            //Buscar color
+            let colorObject = backgroundColors.find(element => element.activity === activityLowerCase);        
+    
+            if(activityLowerCase === 'self care'){ activityLowerCase = 'self-care'}
+    
+            secondSection.innerHTML +=
+    
+                `<div class="card">
+                    <div class="card__background" style="background-color: ${colorObject.color};">
+                        <img class="card__image" src="./images/icon-${activityLowerCase}.svg" alt="${activityLowerCase}-ico" />
                     </div>
-                    <div class="card__time">
-                        <p class="card__hours">${element.current}hrs</p>
-                        <p class="card__previous">Previous - ${element.previous}hrs</p>
+                    <div class="card__details">
+                        <div class="card__activity">
+                            <p class="card__title">${activity}</p>
+                            <img src="./images/icon-ellipsis.svg" alt="menu three dots" />
+                        </div>
+                        <div class="card__time">
+                            <p class="card__hours">${element.current}hrs</p>
+                            <p class="card__previous">Previous - ${element.previous}hrs</p>
+                        </div>
                     </div>
-                </div>
-            </div>`        
-
-    });
+                </div>`        
+    
+        });
+    }    
+window.onload = () => {
+    let array = dailyArray;
+    drawElements(array);
 }
+
